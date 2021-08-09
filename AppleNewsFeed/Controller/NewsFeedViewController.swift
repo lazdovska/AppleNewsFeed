f128bf3e2688947803b09a24656690b8929359b0
lazdovska <lazdovska.kristine@gmail.com>
2021-08-09 22:20:08 +0300

    init AppleNewsFeed project in class

---
+++ AppleNewsFeed/Controller/NewsFeedViewController.swift
@@ -0,0 +1,118 @@
+//
+//  NewsFeedViewController.swift
+//  AppleNewsFeed
+//
+//  Created by kristine.lazdovska on 09/08/2021.
+//
+
+import UIKit
+import Gloss
+
+class NewsFeedViewController: UIViewController {
+
+    var items: [Item] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Apple News"
+        activityIndicatorView.isHidden = true
+    }
+
+    func activityIndicator(animated: Bool){
+        DispatchQueue.main.async {
+            if animated{
+                self.activityIndicatorView.isHidden = false
+                self.activityIndicatorView.startAnimating()
+            }else{
+                self.activityIndicatorView.isHidden = true
+                self.activityIndicatorView.stopAnimating()
+            }
+        }
+    }
+    
+    @IBAction func infoBarItem(_ sender: Any) {
+        basicAlert(title: "News Feed Info!", message: "Press plane to fetch Apple News Feed articles.")
+    }
+    
+    @IBAction func getDataTapped(_ sender: Any) {
+        handleGetData()
+        
+    }
+    
+    func handleGetData(){
+        let jsonUrl = "https://newsapi.org/v2/everything?q=apple&from=2021-08-01&to=2021-08-08&sortBy=popularity&apiKey=32dacd77940549428c61f202bd523f5f"
+        
+        guard let url = URL(string: jsonUrl) else {return}
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-type")
+        
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: urlRequest) { data, response, err in
+        
+            if let err = err {
+                self.basicAlert(title: "Error!", message: "\(err.localizedDescription)")
+            }
+            guard let data = data else{
+                self.basicAlert(title: "Error!", message: "Something went wrong, no data")
+                return
+            }
+            do{
+                if let dictData = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
+                    print("dictData", dictData)
+                    self.populateData(dictData)
+                }
+            }catch{
+            }
+        }
+        task.resume()
+    }
+    func populateData(_ dict:[String: Any]){
+        guard let responseDict = dict["articles"] as? [Gloss.JSON] else{
+            return
+    }
+        
+    items = [Item].from(jsonArray: responseDict) ?? []
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.activityIndicator(animated: false)
+        }
+    }
+}
+
+//Mark: UITableViewDelegate, UITableViewDataSource
+extension NewsFeedViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "newsFeed", for: indexPath) as? NewsTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let item = items[indexPath.row]
+        cell.newsTitleLabel.text = item.title
+        cell.newsTitleLabel.numberOfLines = 0
+        
+        if let image = item.image{
+            cell.newsImageView.image = image
+        }
+        let date = String(item.publishedAt.prefix(10))
+        self.title = "Apple News \(date)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
---
+++ AppleNewsFeed/Controller/NewsTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewsTableViewCell.swift
+//  AppleNewsFeed
+//
+//  Created by kristine.lazdovska on 09/08/2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    
+    @IBOutlet weak var newsImageView: UIImageView!
+}
