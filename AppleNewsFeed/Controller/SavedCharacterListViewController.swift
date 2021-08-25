04c0893c175ba46a6e62b7d644d45c38ea6a9902
Kristine Lsx <lazdovska.kristine@gmail.com>
2021-08-25 11:17:46 +0300

    update Search in SavedItems

--- AppleNewsFeed/Controller/SavedCharacterListViewController.swift
+++ AppleNewsFeed/Controller/SavedCharacterListViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 import CoreData
 
 
-class SavedCharacterListViewController: UITableViewController {
+class SavedCharacterListViewController: UITableViewController, ChangeCharacterDelegate {
     
     var savedItems = [Items]()
     var context: NSManagedObjectContext?
+    var find = FindCharacterController()
     
     
     @IBOutlet weak var editButtonTitle: UIBarButtonItem!
@@ -61,7 +62,7 @@ class SavedCharacterListViewController: UITableViewController {
     }
     
     @IBAction func infoButtonTapped(_ sender: Any) {
-        basicAlert(title: "Saved Character Info!", message: "In this section you will find your saved characters. If you decide to delete some of them, you can do it by using \"Edit\" button and click on delete symbol, or alternative way is to pointer on related character and swipe from right side the left, then press \"delete\"!")
+        basicAlert(title: "Saved Character Info!", message: "In this section you will find your saved characters.\n You can search in your saved character list by using the Magnifying glass icon in TabBar and typing the character name.\n The character list can be refreshed after search, by using the Refresh icon in TabBar.\n If you decide to delete some of them, you can do it by using \"Edit\" button and click on delete symbol, or alternative way is to pointer on related character and swipe from right side the left, then press \"delete\"!")
     }
     
     @IBAction func editButtonTapped(_ sender: Any) {
@@ -73,10 +74,36 @@ class SavedCharacterListViewController: UITableViewController {
         }
     }
     
+    func userEnterCharacterName(character: String){
+        print(character)
+        let request: NSFetchRequest<Items> = Items.fetchRequest()
+        request.predicate = NSPredicate(format: "nameText CONTAINS[c] %@", character)
+        
+        do{
+            savedItems = try (context?.fetch(request))!
+            tableView.reloadData()
+            
+        } catch {
+            fatalError("Error in searching Saved Items")
+        }
+        tableView.reloadData()
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "character" {
+            let vc = segue.destination as! FindCharacterController
+            vc.delegate = self
+        }
+    }
+    
+    @IBAction func refreshSavedItems(_ sender: Any) {
+        loadData()
+    }
+    
+    
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         return savedItems.count
     }
     
@@ -89,9 +116,9 @@ class SavedCharacterListViewController: UITableViewController {
         cell.characterNameLabel.text = item.nameText
         cell.characterNameLabel.numberOfLines = 0
         
-            if let imageData = item.image {
-                cell.characterImageView.image = UIImage(data: imageData as Data)
-            }
+        if let imageData = item.image {
+            cell.characterImageView.image = UIImage(data: imageData as Data)
+        }
         return cell
     }
     
@@ -120,7 +147,7 @@ class SavedCharacterListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-
+            
             let deleteAlert = UIAlertController(title: "Delete", message: "Are you sure you want to delete?", preferredStyle: .alert)
             deleteAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
             deleteAlert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {_ in
